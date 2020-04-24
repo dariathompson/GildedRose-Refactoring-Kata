780c61beb376654f3e314c577c2786c11c5bef4a
dariathompson <daria.kambalina@gmail.com>
2020-04-24 14:50:56 +0100

    test for decreasing quality and days to sell pass

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,19 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
-
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("foo");
+  it("should decrease quality and sellIn by 1", function() {
+    let cheese = new Item("Cheese", 2, 2)
+    let gildedRose = new Shop([ cheese ]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(1);
+    expect(items[0].sellIn).toEqual(1);
+  });
+  it("should increase quality if Aged Brie but decrease sellIn", function() {
+    let cheese = new Item("Cheese", 2, 2)
+    let aged_brie = new Item("Aged Brie", 2, 2)
+    let shop = new Shop([ cheese, aged_brie ]);
+    let items_new= shop.updateQuality();
+    expect(items_new[1].quality).toEqual(3);
+    expect(items_new[1].sellIn).toEqual(1);
   });
 
 });
